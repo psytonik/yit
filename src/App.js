7d340765b8516d70531db184993c1ca6ea6f201a
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-02-05 21:50:04 +0200

    yit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
 import Card from "./components/card";
 import Form from "./components/form";
-import {useState} from "react";
 
 
 const App = () => {
+
     const [picture,setPicture] = useState([
         'https://cf.bstatic.com/xdata/images/hotel/max1280x900/197179243.jpg?k=0ec06b1a6dae5b0d9d4d4bbd02cf8e114fadb30e53ae069de4bceccb80e95faf&o=&hp=1',
-        'https://thumbs.dreamstime.com/b/travel-world-landmarks-background-blue-sky-46083021.jpg',
-        'https://thumbs.dreamstime.com/b/happy-couple-love-travel-raised-hands-cliff-happy-couple-love-travel-raised-hands-cliff-norway-man-woman-112188598.jpg'
-    ])
+
+    ]);
     const [colors,setColors] = useState('#90caf9');
     const [category, setCategory] = useState('travel');
     const [title,setTitle] = useState('ביידן הבטיח, רוסיה זעמה: כוחות צבא אמריקניים נחתו במזרח');
