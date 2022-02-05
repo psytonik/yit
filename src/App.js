4cfc62455e7ba5eb3f4809402630963c8495bdde
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-02-05 23:52:14 +0200

    yit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import './App.css';
 import React, {useState} from "react";
 import Card from "./components/card";
 import Form from "./components/form";
+import {
+    defaultAuthor,
+    defaultCategory,
+    defaultColor,
+    defaultDescription,
+    defaultPicture,
+    defaultTitle
+} from "./constants/defaults";
 
 
 const App = () => {
-
-    // make default const
-    const [picture,setPicture] = useState([
-        'https://cf.bstatic.com/xdata/images/hotel/max1280x900/197179243.jpg?k=0ec06b1a6dae5b0d9d4d4bbd02cf8e114fadb30e53ae069de4bceccb80e95faf&o=&hp=1',
-    ]);
-    const [colors,setColors] = useState('#90caf9');
-    const [category, setCategory] = useState('travel');
-    const [title,setTitle] = useState('ביידן הבטיח, רוסיה זעמה: כוחות צבא אמריקניים נחתו במזרח');
-    const [description,setDescription] = useState('כשנתיים וחצי לאחר חיסול מנהיגו הקודם של דאעש, נשיא ארה"ב נשא הצהרה לאומה על הפשיטה הלילה של כוחות קומנדו בכפר במעוז המורדים בסוריה, שבה נהרג היורש אבו');
-    const [author,setAuthor] = useState('אנתוני פינק');
+    const [picture,setPicture] = useState([defaultPicture]);
+    const [colors,setColors] = useState(defaultColor);
+    const [category, setCategory] = useState(defaultCategory);
+    const [title,setTitle] = useState(defaultTitle);
+    const [description,setDescription] = useState(defaultDescription);
+    const [author,setAuthor] = useState(defaultAuthor);
 
   return (
               <div className="app">
