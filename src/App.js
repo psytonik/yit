d07ba7ab9e321de212d4506cc0482e7769cac559
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-02-05 22:03:39 +0200

    yit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Form from "./components/form";
 
 const App = () => {
 
+    // make default const
     const [picture,setPicture] = useState([
         'https://cf.bstatic.com/xdata/images/hotel/max1280x900/197179243.jpg?k=0ec06b1a6dae5b0d9d4d4bbd02cf8e114fadb30e53ae069de4bceccb80e95faf&o=&hp=1',
-
     ]);
     const [colors,setColors] = useState('#90caf9');
     const [category, setCategory] = useState('travel');
